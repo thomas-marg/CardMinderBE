1f99d40b1f3cf85150dd8bafd56e4b701252131a
Margaritis Thomas <thomas.marg@hotmail.com>
2023-11-20 01:27:12 +0100

    Add files via upload

---
+++ CardMinder/src/main/kotlin/com/example/CardMinder/CardMinderApplication.kt
@@ -0,0 +1,11 @@
+package com.example.CardMinder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CardMinderApplication
+
+fun main(args: Array<String>) {
+	runApplication<CardMinderApplication>(*args)
+}
