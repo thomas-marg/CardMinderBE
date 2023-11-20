d8892c9ed28af730644372945d489c322367b5f9
Margaritis Thomas <thomas.marg@hotmail.com>
2023-11-20 01:27:12 +0100

    Add files via upload

---
+++ CardMinder/src/test/kotlin/com/example/CardMinder/CardMinderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.CardMinder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CardMinderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
